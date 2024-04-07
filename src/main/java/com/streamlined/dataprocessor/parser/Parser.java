670fa1a9c7ee7dd84fbebc6a471799233c3be2e6
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-04-07 23:48:42 +0300

    Test case ParserTest converted into parameterized unit test
    2. Test case ParserTest converted into parameterized unit test that
    accepts source document, parses entity list and checks its validity;
    2. Added method loadData(String[] entityLists) of class Parser to parse
    string documents representing entity lists.

--- src/main/java/com/streamlined/dataprocessor/parser/Parser.java
+++ src/main/java/com/streamlined/dataprocessor/parser/Parser.java
@@ -1,19 +1,22 @@
 package com.streamlined.dataprocessor.parser;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
 import java.util.stream.Stream;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -29,12 +32,10 @@ public class Parser<T extends Entity<?>> {
 	private final CollectionType collectionType;
 	private final int numberOfThreads;
 	private final ExecutorService executorService;
-	private final ArrayBlockingQueue<Path> sourceFileQueue;
 
 	public Parser(Class<T> entityClass, int numberOfThreads) {
 		this.numberOfThreads = numberOfThreads;
 		executorService = Executors.newFixedThreadPool(numberOfThreads);
-		sourceFileQueue = new ArrayBlockingQueue<>(SOURCE_FILE_QUEUE_INITIAL_CAPACITY);
 		mapper = new ObjectMapper();
 		mapper.registerModule(new JavaTimeModule());
 		collectionType = mapper.getTypeFactory().constructCollectionType(List.class, entityClass);
@@ -42,27 +43,65 @@ public class Parser<T extends Entity<?>> {
 
 	public Stream<T> loadData(Path dataPath) {
 		try (var pathStream = Files.newDirectoryStream(dataPath, SOURCE_FILE_PATTERN)) {
-			setUpFileQueue(pathStream);
-			List<Future<List<T>>> futures = startParseTasks();
+			final BlockingQueue<Path> sourceFileQueue = new ArrayBlockingQueue<>(SOURCE_FILE_QUEUE_INITIAL_CAPACITY);
+			pathStream.forEach(sourceFileQueue::add);
+			final Callable<List<T>> taskCallable = () -> {
+				List<T> entities = new ArrayList<>();
+				for (Path filePath = null; (filePath = sourceFileQueue.poll()) != null;) {
+					entities.addAll(parseFile(filePath));
+				}
+				return entities;
+			};
+			return executeTasks(taskCallable);
+		} catch (IOException e) {
+			throw new ParseException("Error iterating through directory %s".formatted(dataPath), e);
+		}
+	}
+
+	private List<T> parseFile(Path filePath) {
+		try (var reader = Files.newBufferedReader(filePath)) {
+			return mapper.readValue(reader, collectionType);
+		} catch (IOException e) {
+			throw new ParseException("Error parsing file %s".formatted(filePath.toAbsolutePath()), e);
+		}
+	}
+
+	public Stream<T> loadData(String[] entityLists) {
+		final BlockingQueue<String> sourceDocumentQueue = new ArrayBlockingQueue<>(entityLists.length);
+		sourceDocumentQueue.addAll(Arrays.asList(entityLists));
+		final Callable<List<T>> taskCallable = () -> {
+			List<T> entities = new ArrayList<>();
+			for (String document = null; (document = sourceDocumentQueue.poll()) != null;) {
+				entities.addAll(parseDocument(document));
+			}
+			return entities;
+		};
+		return executeTasks(taskCallable);
+	}
+
+	private List<T> parseDocument(String document) {
+		try {
+			return mapper.readValue(document, collectionType);
+		} catch (JsonProcessingException e) {
+			throw new ParseException("Error parsing document %s".formatted(document), e);
+		}
+	}
+
+	private Stream<T> executeTasks(Callable<List<T>> taskCallable) {
+		try {
+			List<Future<List<T>>> futures = startParseTasks(taskCallable);
 			executorService.shutdown();
 			executorService.awaitTermination(PARSE_TASK_TERMINATION_TIMEOUT_SECONDS, TimeUnit.SECONDS);
 			return collectTaskResults(futures);
-		} catch (IOException e) {
-			throw new ParseException("Error iterating through directory %s".formatted(dataPath), e);
 		} catch (InterruptedException e) {
 			throw new ParseException("Error while waiting for parsing threads", e);
 		}
 	}
 
-	private void setUpFileQueue(DirectoryStream<Path> pathStream) {
-		sourceFileQueue.clear();
-		pathStream.forEach(sourceFileQueue::add);
-	}
-
-	private List<Future<List<T>>> startParseTasks() {
+	private List<Future<List<T>>> startParseTasks(Callable<List<T>> taskCallable) {
 		List<Future<List<T>>> futures = new ArrayList<>();
 		for (int k = 0; k < numberOfThreads; k++) {
-			futures.add(executorService.submit(this::parseTask));
+			futures.add(executorService.submit(taskCallable));
 		}
 		return futures;
 	}
@@ -76,22 +115,6 @@ public class Parser<T extends Entity<?>> {
 			return stream;
 		} catch (InterruptedException | ExecutionException e) {
 			throw new ParseException("Error while waiting for parsing threads", e);
-		}
-	}
-
-	private List<T> parseTask() {
-		List<T> entities = new ArrayList<>();
-		for (Path filePath = null; (filePath = sourceFileQueue.poll()) != null;) {
-			entities.addAll(parseFile(filePath));
-		}
-		return entities;
-	}
-
-	private List<T> parseFile(Path filePath) {
-		try (var reader = Files.newBufferedReader(filePath)) {
-			return mapper.readValue(reader, collectionType);
-		} catch (IOException e) {
-			throw new ParseException("Error parsing file %s".formatted(filePath.toAbsolutePath()), e);
 		}
 	}
 
--- src/test/java/com/streamlined/dataprocessor/parser/ParserTest.java
+++ src/test/java/com/streamlined/dataprocessor/parser/ParserTest.java
@@ -2,17 +2,16 @@ package com.streamlined.dataprocessor.parser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.nio.file.Path;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.streamlined.dataprocessor.entity.Person;
 
 class ParserTest {
 
-	private static final Path TEST_DATA_DIRECTORY = Path.of("src/main/resources");
 	private static final int NUMBER_OF_THREADS = 8;
 
 	private Parser<Person> parser;
@@ -22,9 +21,158 @@ class ParserTest {
 		parser = new Parser<>(Person.class, NUMBER_OF_THREADS);
 	}
 
-	@Test
-	void testParseFile() {
-		var parsedData = parser.loadData(TEST_DATA_DIRECTORY).toList();
+	@ParameterizedTest
+	@ValueSource(strings = { """
+				[
+				{
+					"name": "John Smith",
+					"birthday": "1970-01-01",
+					"sex": "MALE",
+					"eyeColor": "GREEN",
+					"hairColor": "BLACK",
+					"weight": 60.20,
+					"height": 185.00,
+					"countryOfOrigin": {
+						"name": "Great Britain",
+						"continent": "EUROPE",
+						"capital": "London",
+						"population": 60000000,
+						"square": 1745813.01
+					},
+					"citizenship": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"favoriteMeals":"potatoes,meat,fish"
+				},
+				{
+					"name": "Jacky Bauer",
+					"birthday": "1980-06-20",
+					"sex": "FEMALE",
+					"eyeColor": "BLUE",
+					"hairColor": "YELLOW",
+					"weight": 55.50,
+					"height": 165.00,
+					"countryOfOrigin": {
+						"name": "Germany",
+						"continent": "EUROPE",
+						"capital": "Berlin",
+						"population": 75000000,
+						"square": 1279813.01
+					},
+					"citizenship": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"favoriteMeals":"meat,fish,pasta"
+				},
+				{
+					"name": "Richard Stonewall",
+					"birthday": "1965-07-15",
+					"sex": "MALE",
+					"eyeColor": "BROWN",
+					"hairColor": "GRAY",
+					"weight": 120.20,
+					"height": 170.00,
+					"countryOfOrigin": {
+						"name": "New Zealand",
+						"continent": "AUSTRALIA",
+						"capital": "Wellington",
+						"population": 15000000,
+						"square": 998817.01
+					},
+					"citizenship": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"favoriteMeals":"fish,pasta,apple"
+				},
+				{
+					"name": "Sandy Fisher",
+					"birthday": "1986-12-01",
+					"sex": "FEMALE",
+					"eyeColor": "BLUE",
+					"hairColor": "GRAY",
+					"weight": 75.00,
+					"height": 175.00,
+					"countryOfOrigin": {
+						"name": "Canada",
+						"continent": "NORTH_AMERICA",
+						"capital": "Ottawa",
+						"population": 125000000,
+						"square": 6345813.01
+					},
+					"citizenship": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"favoriteMeals":"pasta,apple,pear"
+				},
+				{
+					"name": "Ken Winston",
+					"birthday": "2000-04-10",
+					"sex": "MALE",
+					"eyeColor": "RED",
+					"hairColor": "GREEN",
+					"weight": 80.50,
+					"height": 205.00,
+					"countryOfOrigin": {
+						"name": "Netherlands",
+						"continent": "EUROPE",
+						"capital": "Amsterdam",
+						"population": 35000000,
+						"square": 425853.01
+					},
+					"citizenship": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"favoriteMeals":"apple,pear,grape"
+				},
+				{
+					"name": "Sunny Smile",
+					"birthday": "2010-03-05",
+					"sex": "FEMALE",
+					"eyeColor": "BROWN",
+					"hairColor": "YELLOW",
+					"weight": 50.20,
+					"height": 130.00,
+					"countryOfOrigin": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"citizenship": {
+						"name": "USA",
+						"continent": "NORTH_AMERICA",
+						"capital": "Washington",
+						"population": 250000000,
+						"square": 6361952.20
+					},
+					"favoriteMeals":"pear,grape,meat"
+				}
+			]
+
+						""" })
+	void testParseFile(String entityList) {
+		var parsedData = parser.loadData(new String[] { entityList }).toList();
 
 		assertEquals(6, parsedData.size());
 
@@ -48,7 +196,6 @@ class ParserTest {
 		assertEquals("pasta,apple,pear", parsedData.get(3).getFavoriteMeals());
 		assertEquals("apple,pear,grape", parsedData.get(4).getFavoriteMeals());
 		assertEquals("pear,grape,meat", parsedData.get(5).getFavoriteMeals());
-
 	}
 
 }
